003af7ac8f18ecf276877e6912d61f1d18e2dc1c
admin <admin@qq.com>
2020-12-03 11:29:32 +0800

    Initial commit

---
+++ src/main/java/com/zzcedu/wodeshouxuan/WodeshouxuanApplication.java
@@ -0,0 +1,13 @@
+package com.zzcedu.wodeshouxuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WodeshouxuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WodeshouxuanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzcedu/wodeshouxuan/WodeshouxuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzcedu.wodeshouxuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WodeshouxuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
